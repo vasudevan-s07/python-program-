559f74601c445da144c43e256ff472efc333b880
vasudevan-s07 <66818210+vasudevan-s07@users.noreply.github.com>
2020-06-17 15:12:23 +0530

    Create positive number program
    This file print postive number in list

---
+++ Print postive number.py
@@ -0,0 +1,11 @@
+#python program to print postive Number in a List
+
+#list of numbers 
+List1 =[11, -23, 45, -64, 93 ]
+
+#iterating each number in list 
+for num in List1:
+    # checking condition 
+    if num>=0:
+    
+    print (num,end=" ")
